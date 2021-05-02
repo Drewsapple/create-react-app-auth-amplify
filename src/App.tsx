d25169c79d11e9418779b3c0d28f7680682358b7
Drew Fisher <awfisher@wpi.edu>
2021-05-02 18:56:30 -0400

    reenable cookies

--- src/App.tsx
+++ src/App.tsx
@@ -34,13 +34,13 @@ Amplify.configure({
     identityPoolRegion: aws_exports.aws_cognito_region,
     userPoolId: aws_exports.aws_user_pools_id,
     userPoolWebClientId: aws_exports.aws_user_pools_web_client_id,
-    // mandatorySignIn: true,
-    // cookieStorage: {
-    //   domain: 'tracking.boop.sh',
-    //   path: '/',
-    //   expires: 365,
-    //   secure: process.env.NODE_ENV !== 'development',
-    // }
+    mandatorySignIn: true,
+    cookieStorage: {
+      domain: 'tracking.boop.sh',
+      path: '/',
+      expires: 365,
+      secure: process.env.NODE_ENV !== 'development',
+    }
   },
   "aws_appsync_authenticationType": aws_exports.aws_appsync_authenticationType,
   "aws_appsync_graphqlEndpoint": aws_exports.aws_appsync_graphqlEndpoint,
